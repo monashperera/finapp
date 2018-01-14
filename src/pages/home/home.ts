d50b92a2a222530a537e9d76ccc59b2c91cbf1c6
monashperera <monashperera@gmail.com>
2018-01-14 14:42:08 +0530

    ytt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,16 @@ import { FingerprintAIO, FingerprintOptions} from'@ionic-native/fingerprint-aio'
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  fingerprintOptions: FingerprintOptions
   constructor(private fingerprint: FingerprintAIO, private platform: Platform ) {
+    this.fingerprintOptions = {
 
+      clientId: 'Fingerprint-Demo',
+    clientSecret: 'password', //Only necessary for Android
+    disableBackup:true,  //Only for Android(optional)
+    localizedFallbackTitle: 'Use Pin', //Only for iOS
+    localizedReason: 'Please authenticate' //Only for iOS
+    }
   
 
   }
@@ -19,6 +26,10 @@ export class HomePage {
       await this.platform.ready();
       const available = await this.fingerprint.isAvailable();
       console.log(available);
+      if( available === "OK"){
+        const result = await this.fingerprint.show(this.fingerprintOptions);
+        console.log(result);
+      }
     }
     catch(e){
       console.error(e);
@@ -26,4 +37,4 @@ export class HomePage {
   }
 
 }
-  
+ 
